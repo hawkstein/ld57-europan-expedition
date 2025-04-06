48bff4c92429009a75d0a73d54e52c89d9679738
Liam Hawks <68058931+hawkstein@users.noreply.github.com>
2025-04-06 17:57:36 +0100

    decide to remove forge from the game

--- scripts/interlude.gd
+++ scripts/interlude.gd
@@ -1,11 +1,12 @@
 extends Control
 
 @onready var intro: Label = $VBoxContainer/Intro
-const FORGE = preload("res://scenes/forge.tscn")
+#const FORGE = preload("res://scenes/forge.tscn")
+const GAME = preload("res://scenes/game.tscn")
 
 func _ready() -> void:
 	intro.text = GameManager.get_intro()
 
 
 func _on_continue_pressed() -> void:
-	get_tree().change_scene_to_packed(FORGE)
+	get_tree().change_scene_to_packed(GAME)
