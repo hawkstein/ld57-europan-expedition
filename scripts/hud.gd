ab9f74f863fa6cf02894a1573c58be27c4757183
Liam Hawks <68058931+hawkstein@users.noreply.github.com>
2025-04-07 11:15:53 +0100

    update game ui with days

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -2,10 +2,12 @@ extends CanvasLayer
 
 @onready var energy_label: Label = $EnergyLabel
 @onready var materials_label: Label = $MaterialsLabel
+@onready var day_label: Label = $DayLabel
 
 func _ready() -> void:
 	materials_label.text = "Materials: "+str(GameManager.player_ore)
 	GameManager.connect("ore_updated", on_ore_update)
+	day_label.text = GameManager.get_day()
 
 func _on_submersible_energy_update(energy: float) -> void:
 	energy_label.text = "Energy: "+str(energy).pad_decimals(1)
